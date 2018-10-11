25e871e01182daa46ba7b8ab3417fc79498102a3
brookmezgu <brookmezgebu64@gmail.com>
2018-10-11 22:00:47 +0300

    -- setting up the react ... deleting everything and making a basic struct

---
+++ front_end/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import '../styles/App.css'
+
+class App extends Component {
+
+    render() {
+        return (
+          <div className="App-title">
+              <h3> Welcome To RAudio. </h3>
+          </div>
+        )
+    }
+}
+
+export default App;
