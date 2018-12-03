c3999e2c8ded70d6a6338158e06ca4e33c97523b
brookmezgu <brookmezgebu64@gmail.com>
2018-12-03 12:20:55 +0300

    [PROGRESS]
    + fixed Access-Control-Allow-Origin problem when requesting data from server

--- Connector/server/server.go
+++ Connector/server/server.go
@@ -2,10 +2,10 @@ package server
 
 import (
 	"../indexi"
-	"../mux-master"
 	"bytes"
-	"fmt"
-	"github.com/mikkyang/id3-go"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	//"github.com/mikkyang/id3-go"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,12 +13,14 @@ import (
 )
 
 func AllListHandler(x http.ResponseWriter, b *http.Request){
+	enableCors(&x)
 	list := indexi.GetMusicList()
 	json := list.ToJson()
 	x.Write(stringToByteSlice(json))
 }
 
 func RefreshedListHandler (x http.ResponseWriter , b *http.Request) {
+	enableCors(&x)
 	list := indexi.GetRefreshedMusicList()
 	json := list.ToJson()
 	x.Write(stringToByteSlice(json))
@@ -28,31 +30,54 @@ func stringToByteSlice (convertable string) []byte {
 	return bytes.Trim([]byte(convertable), "\x00")
 }
 
-func _ (x string) {
-	music , err := id3.Open(x)
+//func testMp3Loading (x string) {
+//	music , err := id3.Open(x)
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	fmt.Println("Music Artist : " , music.Artist())
+//	fmt.Println("Music Album : " , music.Album())
+//	fmt.Println("Music Genre : " , music.Genre())
+//	fmt.Println("Music Year : " , music.Year())
+//
+//}
 
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Music Artist : " , music.Artist())
-	fmt.Println("Music Album : " , music.Album())
-	fmt.Println("Music Genre : " , music.Genre())
-	fmt.Println("Music Year : " , music.Year())
-
+type Command struct {
+	Com string `json:"com"`
+	Fun string `json:"fun"`
 }
 
+type Commands []Command
+
 func PrintHelp (x http.ResponseWriter , b *http.Request) {
-	x.Write(stringToByteSlice("{\n \"/limited_list/[NUM]\" : \"gives json of [NUM] elements\" ,\n \"/refreshed\" : \"gives json of refreshed list\",\n \"/list\" : \"gives json of all the list items\"\n}"))
+	var commands Commands
+	enableCors(&x)
+
+	command := Command{Com: "/limited_list/[NUM]", Fun: "gives json of [NUM] elements"}
+	commands = append(commands , command)
+	command = Command{Com: "/refreshed", Fun: "gives json of refreshed list"}
+	commands = append(commands , command)
+	command = Command{Com: "/list", Fun: "gives json of all the list items"}
+	commands = append(commands , command)
+	command = Command{Com: "/search/[STRING]", Fun: "search from stuff"}
+	commands = append(commands , command)
+
+	commandsJson , _ := json.Marshal(commands)
+	x.Write(stringToByteSlice(string(commandsJson)))
+	//x.Write(stringToByteSlice("{\n \"/limited_list/[NUM]\" : \"gives json of [NUM] elements\" ,\n \"/refreshed\" : \"gives json of refreshed list\",\n \"/list\" : \"gives json of all the list items\"\n}"))
 }
 
 func SearchMusicName (x http.ResponseWriter , b *http.Request) {
+	enableCors(&x)
 	searchQuery := strings.Replace(b.URL.Path , "/search/" , "" ,-1)
 	list := indexi.GetMusicList()
 	x.Write( stringToByteSlice( list.Search(searchQuery).ToJson()))
 }
 
 func ListLimited(x http.ResponseWriter , b * http.Request) {
+	enableCors(&x)
 	list := indexi.GetMusicList()
 	frag := strings.Replace(b.URL.Path , "/list_limited/" , "" , -1)
 	limit , err := strconv.Atoi(frag)
@@ -67,12 +92,17 @@ func ListLimited(x http.ResponseWriter , b * http.Request) {
 	x.Write(stringToByteSlice(json))
 }
 
+func enableCors ( rw *http.ResponseWriter) {
+	(*rw).Header().Set("Access-Control-Allow-Origin" , "*")
+}
+
 func StartServer () {
 	r := mux.NewRouter()
 	r.HandleFunc("/help", PrintHelp)
 	r.HandleFunc("/list" , AllListHandler)
 	r.HandleFunc("/refreshed" , RefreshedListHandler)
 	r.HandleFunc("/list_limited/{size:[0-9]+}" , ListLimited)
+	//testMp3Loading("C:\\one.mp3")
 	r.HandleFunc("/search/{name:[a-zA-Z0-9 ]+}" , SearchMusicName)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8002" , r))
 }
--- front_end/src/components/App.jsx
+++ front_end/src/components/App.jsx
@@ -4,6 +4,7 @@ import '../styles/App.css'
 class App extends Component {
 
     render() {
+        fetch ("http://localhost:8002/help").then(response => console.log(response));
         return (
           <div className="App-title">
               <h3> Welcome To RAudio. </h3>
